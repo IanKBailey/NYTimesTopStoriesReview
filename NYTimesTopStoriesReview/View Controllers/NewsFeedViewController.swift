e9315d13a707acb3ee26468e14e6930338fbeb25
Ian Bailey <iankbailey2@gmail.com>
2020-02-06 16:07:54 -0500

    Initial Commit

---
+++ NYTimesTopStoriesReview/View Controllers/NewsFeedViewController.swift
@@ -0,0 +1,54 @@
+//
+//  NewsFeedViewController.swift
+//  NYTimesTopStoriesReview
+//
+//  Created by Ian Bailey on 2/6/20.
+//  Copyright © 2020 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+     private let newsFeedView = NewsFeedView()
+      
+      override func loadView() {
+        view = newsFeedView
+      }
+      
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground // white when dark mode is off, black when dark mode is on
+        
+        // setting up collection datasource and delegate
+        newsFeedView.collectionView.dataSource = self
+        newsFeedView.collectionView.delegate = self
+        
+        // register a collection view cell
+        newsFeedView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "articleCell")
+      }
+    }
+
+    extension NewsFeedViewController: UICollectionViewDataSource {
+      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+      }
+      
+      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "articleCell", for: indexPath)
+        cell.backgroundColor = .white
+        return cell
+      }
+    }
+
+    extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+      // return item size
+      // itemHeight ~ 30% of height of device
+      // itemWidth = 100% of width of device
+      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.30 // 30%
+        return CGSize(width: itemWidth, height: itemHeight)
+      }
+    }
---
+++ NYTimesTopStoriesReview/View Controllers/SavedArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SavedArticleViewController.swift
+//  NYTimesTopStoriesReview
+//
+//  Created by Ian Bailey on 2/6/20.
+//  Copyright © 2020 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class SavedArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
